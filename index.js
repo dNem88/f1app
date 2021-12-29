60f5eca1f8521dd1f96916528e51ffe1d7665465
dNem88 <82020442+dNem88@users.noreply.github.com>
2021-12-29 15:45:28 +0200

    dotenv

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const app = express();
 const router = require('./routes');
 const path = require('path');
 
-require('dotenv').config({
-    path: path.resolve(process.cwd(), 'config/.env')
-});
+// require('dotenv').config({
+//     path: path.resolve(process.cwd(), 'config/.env')
+// });
 require('./middlewares/config')(app);
 require('./config/database')(app);
 app.use(router);
 
-app.listen(process.env.PORT , console.log(`Server listens on port ${process.env.PORT}`));
+app.listen(process.env.PORT, console.log(`Server listens on port ${process.env.PORT}`));
 
 
