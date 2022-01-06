fc42070fb35fe379a48566ca38c12a1a9374968c
dNem88 <82020442+dNem88@users.noreply.github.com>
2022-01-06 09:37:50 +0200

    .env

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const router = require('./routes');
 const path = require('path');
 
-require('dotenv').config({
-    path: path.resolve(process.cwd(), 'config/.env')
-});/*must not be included in production*/
+// require('dotenv').config({
+//     path: path.resolve(process.cwd(), 'config/.env')
+// });/*must not be included in production*/
 require('./middlewares/config')(app);
 require('./config/database')(app);
 app.use(router);
