2d6b109d869c9edeab743fe5688936020e21c762
dNem88 <82020442+dNem88@users.noreply.github.com>
2022-09-07 12:04:37 +0300

    db uri

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const router = require('./routes');
 const path = require('path');
 
 
-    require('dotenv').config({
-        path: path.resolve(process.cwd(), 'config/.env')
-    });/*must not be included in production*/
+    // require('dotenv').config({
+    //     path: path.resolve(process.cwd(), 'config/.env')
+    // });/*must not be included in production*/
 
 
 require('./middlewares/config')(app);
 require('./config/database')(app);
 app.use(router);
 
-app.listen(process.env.PORT || 3020, console.log(`Server listens on port ${process.env.PORT}`));
+;
 
 // module.exports = app; /*This is made for testing with JEST*/
 
